6e2df639a58a18eca4c95e06aa25f755387a060a
PabloBenavides <pblo.benavides@gmail.com>
2019-08-27 11:35:46 -0500

    Cambios:
    - Clase auxiliar para los datos de correo.

---
+++ src/main/java/com/grucas/domain/config/_Email.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grucas.domain.config;
+
+/**
+ *
+ * @author GrucasDev
+ */
+public class _Email {
+    
+    public static String HOST = "email.grucas.com";
+    public static String PORT = "465";
+    public static String USERNAME = "notificaciones@grucas.com";
+    public static String PASSWORD = "noti_grucas$";
+    public static String SSL = "false";
+    public static String STARTTLS = "false";
+    public static String SERVER_SSL = "";
+    public static String FROM = "notificaciones@grucas.com";
+
+}
