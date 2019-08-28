445daab62bdb8586e7b34b4c137ff1b525e28481
PabloBenavides <pblo.benavides@gmail.com>
2019-08-28 12:10:13 -0500

    Features:
    - Modificaciones en la clase _Email para envio de correos con SSL y se añadio el correo de soporte.

--- src/main/java/com/grucas/domain/config/_Email.java
+++ src/main/java/com/grucas/domain/config/_Email.java
@@ -15,9 +15,13 @@ public class _Email {
     public static String PORT = "465";
     public static String USERNAME = "notificaciones@grucas.com";
     public static String PASSWORD = "noti_grucas$";
-    public static String SSL = "false";
+    public static String SSL = "true";
     public static String STARTTLS = "false";
     public static String SERVER_SSL = "";
     public static String FROM = "notificaciones@grucas.com";
+    
+    public static String SERVICE_DESK_USERNAME = "soporte@grucas.com";
+    public static String SERVICE_DESK_PASSWORD = "s0p0rtedac";
+    public static String SERVICE_DESK_FROM = "soporte@grucas.com";
 
 }
