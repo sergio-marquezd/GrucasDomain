dfa832755cc720ea2cce25dd7c7c9c9c9d8fc786
PabloBenavides <pblo.benavides@gmail.com>
2019-09-24 10:14:23 -0500

    Updates:
    - Clases para el llenado de formatos y area comercial

---
+++ src/main/java/com/grucas/domain/model/AgenteAduanal.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grucas.domain.model;
+
+import com.rubik.Base.ObjectBase;
+
+/**
+ *
+ * @author GrucasDev
+ */
+public class AgenteAduanal extends ObjectBase {
+    
+    String nombre;
+    String patente;
+
+    public AgenteAduanal() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+}
